4e45e1a09a1f08a82cfad8f2ea31183df3c722f1
Eduard Kerkhoven <eduardk@chalmers.se>
2021-02-19 00:33:00 +0100

    fix: changeMedia_batch blocks relevant c-source

--- customGECKO/changeMedia_batch.m
+++ customGECKO/changeMedia_batch.m
@@ -24,20 +24,20 @@ exchange           = exchange(find(contains(rxnIDs,'_REV')));
 model.ub(exchange) = 0;
 %Allow main carbon source uptake
 c_id  = model.rxns(strcmp(model.rxnNames,c_source));
-model = setParam(model,'ub',c_id,Inf);
-%block xylose and oxygen production
-model.ub(strcmp(model.rxnNames,'oxygen exchange'))    = 0;
-model.ub(strcmp(model.rxnNames,'D-xylose exchange')) = 0;
+model = setParam(model,'ub',c_id,1000);
+%block carbon source and oxygen production
+model.ub(strcmp(model.rxnNames,'oxygen exchange')) = 0;
+model.ub(strcmp(model.rxnNames,regexprep(c_source,' \(reversible\)$',''))) = 0;
 %Allow uptake of essential components
-model = setParam(model, 'ub', 'r_1654_REV', Inf); % 'ammonium exchange';
-model = setParam(model, 'ub', 'r_2100_REV', Inf); % 'water exchange' ;
-model = setParam(model, 'ub', 'r_1992_REV', Inf); % 'oxygen exchange';
-model = setParam(model, 'ub', 'r_2049_REV', Inf); % sodium exchange
-model = setParam(model, 'ub', 'r_1861_REV', Inf); % 'iron(2+) exchange';
-model = setParam(model, 'ub', 'r_2005_REV', Inf); % 'phosphate exchange';
-model = setParam(model, 'ub', 'r_2060_REV', Inf); % 'sulphate exchange';
-model = setParam(model, 'ub', 'r_1832_REV', Inf); % 'H+ exchange' ;
-model = setParam(model, 'ub', 'r_2020_REV', Inf); % potassium exchange
+model = setParam(model, 'ub', 'r_1654_REV', 1000); % 'ammonium exchange';
+model = setParam(model, 'ub', 'r_2100_REV', 1000); % 'water exchange' ;
+model = setParam(model, 'ub', 'r_1992_REV', 1000); % 'oxygen exchange';
+model = setParam(model, 'ub', 'r_2049_REV', 1000); % sodium exchange
+model = setParam(model, 'ub', 'r_1861_REV', 1000); % 'iron(2+) exchange';
+model = setParam(model, 'ub', 'r_2005_REV', 1000); % 'phosphate exchange';
+model = setParam(model, 'ub', 'r_2060_REV', 1000); % 'sulphate exchange';
+model = setParam(model, 'ub', 'r_1832_REV', 1000); % 'H+ exchange' ;
+model = setParam(model, 'ub', 'r_2020_REV', 1000); % potassium exchange
 %Allow biomass production 
-model = setParam(model, 'ub', 'r_2111', Inf); % growth
+model = setParam(model, 'ub', 'r_2111', 1000); % growth
 end

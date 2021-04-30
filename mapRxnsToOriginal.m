184c38bb7953a8a039f8a4bd61bbfe1f2cd7f0ea
Eduard Kerkhoven <eduardk@chalmers.se>
2021-05-01 00:31:06 +0200

    feat: mapRxnsToOriginal
    - when interested in the fluxes through the metabolic reactions (and not enzyme usages), mapRxnsToOriginal can map the fluxes back to the reactions in the original non-ecModel.

---
+++ mapRxnsToOriginal.m
@@ -0,0 +1,70 @@
+function solXmapped = mapRxnsToOriginal(ecModel,model,solX)
+% mapRxnsToOriginal
+%
+%   Maps fluxes as obtained from an ecModel to the reactions as they were
+%   defined in the non-ecModel that forms the basis for the ecModel. If
+%   used for further summarizing of the data, use model to provide the
+%   model structure.
+%
+%   Input:
+%   ecModel     model structure of enzyme-constrained model that was
+%               simulated
+%   model       model structure of the classical, non-ecModel that was used
+%               to construct the ecModel
+%   solX        vector with flux solution from simulation with ecModel
+%
+%   Output:
+%   solXmapped  vector with flux solution after mapping to the original
+%               model
+%
+%   Example:
+%   sol = solveLP(ecModel);
+%   solX = sol.x;
+%   solXmapped = mapsRxnsToOriginal(ecModel,model.solX);
+%   printFluxes(model,solXmapped);
+%
+%   Usage: solXmapped = mapRxnsToOriginal(ecModel,model,solX)
+
+rxns = ecModelP_ANlim.rxns;
+sol=solveLP(ecModelP_ANlim);
+solX=sol.x;
+%Unchanged reactions
+oriRxns = cellfun(@isempty,regexp(rxns,'(^arm_)|(^(draw_)?prot)|(No\d{1,2}$)'));
+
+%Remove protein exchange reactions
+protEx = ~cellfun(@isempty,regexp(rxns,'^(draw_)?prot_'));
+rxns(protEx) = [];
+solX(protEx) = [];
+
+%Keep arm reactions, remove arm_ prefix
+armRxns         = find(~cellfun(@isempty,regexp(rxns,'^arm_.+')));
+armRxnIds       = regexprep(rxns(armRxns),'^arm_','');
+rxns(armRxns)   = armRxnIds;
+
+%Remove arm-extension reactions
+rmArmRxns = contains(rxns,armRxnIds);
+rxns(rmArmRxns) = [];
+solX(rmArmRxns) = [];
+
+%Merge rev reactions
+revRxns     = find(contains(rxns,'_REV'));
+revRxnsOri  = regexprep(rxns(revRxns),'_REV','');
+[revMatch,fwdRxns] = ismember(revRxnsOri,rxns);
+fwdRxns     = fwdRxns(revMatch);
+revRxns     = revRxns(revMatch);
+solX(fwdRxns) = solX(fwdRxns) - solX(revRxns);
+solX(revRxns) = [];
+rxns(revRxns) = [];
+
+%Remove No1 suffix from single-enzyme reactions
+rxns = regexprep(rxns,'No1$','');
+
+%Match to original model
+solXmapped = zeros(numel(model.rxns),1);
+[a,b] = ismember(rxns,model.rxns);
+solXmapped(b(a)) = solX(a);
+
+
+
+
+

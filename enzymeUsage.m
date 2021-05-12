3bb83d7896d15393798b426d246fc1272db1634e
alinarekena <60570315+alinarekena@users.noreply.github.com>
2021-05-12 11:09:12 +0300

    fix: refactor results back to non-ecModel

--- enzymeUsage.m
+++ enzymeUsage.m
@@ -1,4 +1,4 @@
-function [absUsage,capUsage,UB,protId] = enzymeUsage(ecModel,fluxes,zero)
+function [absUsage,capUsage,UB,protId] = enzymeUsage(ecModel,fluxes,zero,contrastRevRxns)
 % enzymeUsage
 %
 %   Gives enzyme usages based on a provided flux distribution. It can give:
@@ -17,6 +17,11 @@ function [absUsage,capUsage,UB,protId] = enzymeUsage(ecModel,fluxes,zero)
 %   fluxes          vector of fluxes, for instance sol.x
 %   zero            logical whether also zero absolute enzyme usages should be
 %                   included (opt, default true)
+%   contrastRevRxns logical whether usage of enzymes involved in reversible
+%                   reactions should be subtracted from either direction,
+%                   to yield "net" usage. If set to false, usage is defined
+%                   from the enzyme exchange reactions instead (opt,
+%                   default false)
 %
 %   Output:
 %   capUsage        vector of enzyme capacity usages
@@ -24,37 +29,59 @@ function [absUsage,capUsage,UB,protId] = enzymeUsage(ecModel,fluxes,zero)
 %   UB              vector of enzyme exchange reaction upper bounds
 %   protId          string array of protein IDs matching the other output
 %
-% Usage: [absUsage,capUsage,UB,protId] = enzymeUsage(ecModel,fluxes,zero)
+% Usage: [absUsage,capUsage,UB,protId] = enzymeUsage(ecModel,fluxes,zero,contrastRevRxns)
 
 if nargin<3
     zero=true;
 end
-
-if isfield(ecModel,'enzymes')
-    protIdx     = find(contains(ecModel.rxnNames,ecModel.enzymes));
-    matchProt   = regexprep(ecModel.rxnNames(protIdx),'(draw_)?prot_','');
-    matchProt   = regexprep(matchProt,'_exchange.*','');
-    [~,b]       = ismember(ecModel.enzymes, matchProt);
-
-    absUsage    = fluxes(protIdx);
-    UB          = ecModel.ub(protIdx);
-    capUsage    = absUsage./UB;
-   
-    absUsage    = absUsage(b);
-    capUsage    = capUsage(b);
-    UB          = UB(b);
-    protId      = ecModel.enzymes;
-else
-    expression  = '^(draw_prot_.*)|(prot_.*_exchange$)';
-    protRxn     = regexp(ecModel.rxnNames,expression);
-    protRxn     = find(~cellfun(@isempty,protRxn));
-    expression  = '(^draw_prot_)|(^prot_)|(_exchange$)';
-    protId      = regexprep(ecModel.rxnNames(protRxn),expression,'');
-    
-    absUsage    = fluxes(protRxn);
-    UB          = ecModel.ub(protRxn);
-    capUsage    = absUsage./UB;
+if nargin<4
+    contrastRevRxns=false;
 end
+
+%Get enzyme exchange reactions (exclude protein pool)
+expression  = '^(draw_prot_.*)|(prot_[^(pool)].*_exchange$)';
+enzExch     = regexp(ecModel.rxnNames,expression);
+enzExch     = find(~cellfun(@isempty,enzExch));
+
+%Get list of Uniprot protein IDs
+%if ~isfield(ecModel,'enzymes')
+    expression  = '(^draw_prot_)|(^prot_)|(_exchange$)';
+    protId      = regexprep(ecModel.rxnNames(enzExch),expression,'');
+%else
+%    protId      = ecModel.enzymes;
+%end
+
+%Map reversible reactions to their original flux value
+if contrastRevRxns == true
+    revRxns     = find(contains(ecModel.rxns,'_REV'));
+    revRxnsOri  = regexprep(ecModel.rxns(revRxns),'_REV','');
+    [revMatch,oriRxns] = ismember(revRxnsOri,ecModel.rxns);
+    oriRxns     = oriRxns(revMatch);
+    revRxns     = revRxns(revMatch);
+    fluxes(oriRxns) = fluxes(oriRxns) - fluxes(revRxns);
+else
+    revRxns = false(numel(ecModel.rxns),1);
+end
+
+%Index of enzymes in S-matrix
+enzMets     = strcat('prot_',protId);
+[~,enzMets] = ismember(enzMets,ecModel.mets);
+%Index of reactions that use enzymes
+[~,enzRxns,~]   = find(ecModel.S(enzMets,:));
+enzRxns   = unique(enzRxns);
+%Leave out exchange and reversed reactions
+enzRxns   = setdiff(enzRxns,enzExch,'sorted');
+enzRxns   = setdiff(enzRxns,revRxns,'sorted');
+
+%Calculate absolute usage
+enzFlux = -full(ecModel.S(enzMets,enzRxns)) * fluxes(enzRxns);
+%Take absolute value, as net negative flux through reversible reaction
+%should still yield a positive enzyme usage
+absUsage = abs(enzFlux);
+
+%For capacity usage
+UB = ecModel.ub(enzExch);
+capUsage = absUsage ./ UB;
 
 if true(~zero)
     nonzero     = absUsage>0;
